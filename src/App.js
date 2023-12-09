86b3d9de948dfc4cd9717b01facbc8422abef246
ravikumarmunikuntala8 <153378860+ravikumarmunikuntala8@users.noreply.github.com>
2023-12-09 23:34:10 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Landing from "./pages/Landing";
 import Platform from "./pages/Platform";
-import Login from "./pages/Login";https://github.com/ravikumarmunikuntala8/ott-platform-official/blob/main/src/App.js
+import Login from "./pages/Login";
 import FacebookLogin from "./pages/FacebookLogin";
 import TwitterLogin from "./pages/TwitterLogin";
 import ThankYou from "./pages/ThankYou";
